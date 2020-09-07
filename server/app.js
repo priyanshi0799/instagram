25200ef323f8ccc6d4b2bd580e246305b56a84a1
Priyanshi Gupta <priyanshigupta0708@gmail.com>
2020-09-07 23:22:34 +0530

    Signup route created

--- server/app.js
+++ server/app.js
@@ -6,15 +6,18 @@ const { MONGOURI } = require('./keys')
 
 require('./models/user')
 
+app.use(express.json())
 app.use(require('./routes/auth'))
 
 mongoose.connect(MONGOURI , {
     useNewUrlParser : true,
     useUnifiedTopology : true
 })
+
 mongoose.connection.on('connected',() => {
     console.log('Connected to MongoDB')
 })
+
 mongoose.connection.on('error',(err) => {
     console.log('Error ', err)
 })
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,47 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const router = express.Router()
+
+const User = mongoose.model('User')
 
 router.get('/', (req,res) => {
     res.send('Home Route')
+})
+
+router.post('/signup' , (req,res) => {
+    const { name , email , password} = req.body
+    if(!email || !password || !name){
+        return res.status(422).json({
+            error : 'Please add all the fields'
+        })
+    }
+
+    User.findOne({email : email})
+        .then((savedUser) => {
+            if(savedUser){
+                return res.status(422).json({
+                    error : 'User already exists'
+                })
+            }else{
+                const user = new User({
+                    email: email,
+                    password : password,
+                    name : name
+                })
+                user.save()
+                    .then(user => {
+                        res.json({
+                            message: 'Saved Successfully'
+                        })
+                    })
+                    .catch(err => {
+                        res.send(err)
+                    })
+            }
+        })
+        .catch(err => {
+            res.send(err)
+        })
 })
 
 module.exports = router
